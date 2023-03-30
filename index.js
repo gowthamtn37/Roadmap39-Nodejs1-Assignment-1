ccdcec0250a5f45242c6aaf0225f38f0c3462eef
gowthamtn37 <gowthamtn37@gmail.com>
2023-03-30 19:29:27 +0530

    task completed

---
+++ index.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const path = require("path");
+
+const time = new Date().toLocaleString();
+const today = new Date();
+const current = `Date ${today.getDay()}-${today.getMonth()}-${today.getFullYear()}, Time ${today.getHours()}-${today.getMinutes()}-${today.getSeconds()}`;
+
+fs.writeFile(`./${current}.txt`, time, (err) => {
+  if (err) {
+    console.log(Error);
+  } else {
+    console.log("file created");
+  }
+});
+
+fs.readdir(__dirname, (err, files) => {
+  if (err) console.log(Error);
+  else {
+    console.log("Filenames with the .txt extension:");
+    files.forEach((file) => {
+      if (path.extname(file) == ".txt") console.log(file);
+    });
+  }
+});
+
+//http://localhost:3000
